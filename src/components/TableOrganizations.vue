c018b5ef0c5beb9a72c5ec5bb503a4112a1f9934
Matteo Kocken <matteokocken@gmail.com>
2024-10-03 16:44:41 +0200

    Ajust TS error

--- src/components/TableOrganizations.vue
+++ src/components/TableOrganizations.vue
@@ -13,17 +13,17 @@
     <q-table
       :rows="filteredOrganizations"
       :columns="columns"
-      row-key="id"
+      row-key="_id"
       :filter="search"
       :loading="loading"
     >
       <template v-slot:top-right>
-        <!--<q-btn
+        <q-btn
           color="primary"
           icon="add"
-          label="Nouveau Organization"
+          label="Nouvelle Organisation"
           @click="openAddorganizationDialog"
-        />-->
+        />
       </template>
 
       <template v-slot:body-cell-subscription="props">
@@ -46,7 +46,7 @@
       </template>
     </q-table>
 
-    <!-- Dialogue pour ajouter/éditer un organisation -->
+    <!-- Dialogue pour ajouter/éditer une organisation -->
     <q-dialog v-model="organizationDialog.show" persistent>
       <q-card style="width: 90%; max-width: 1200px">
         <q-card-section>
@@ -58,113 +58,40 @@
         <div class="flex q-gutter-xl">
           <q-card-section style="flex: 1">
             <div class="text-bold q-mb-md">Informations</div>
-            <div class="flex q-gutter-md">
-              <q-input
-                class="q-mb-md"
-                style="flex: 1%"
-                v-model="organizationDialog.organization.organization_name"
-                label="Prénom"
-                outlined
-                dense
-              />
-            </div>
+            <q-input
+              class="q-mb-md"
+              v-model="organizationDialog.organization.organization_name"
+              label="Nom de l'organisation"
+              outlined
+              dense
+            />
+
             <div class="text-bold q-mb-sm">
               Membres de l'organisation ({{ organizationDialog.organization.members.length }}/{{
                 getMaxSeats
               }})
             </div>
             <div class="">
-              <div>
-                <q-chip
-                  v-for="member in organizationDialog.organization.members"
-                  :key="member"
-                  removable
-                  @remove="removeMember(member)"
-                >
-                  {{ member }}
-                </q-chip>
-              </div>
-              <div class="flex q-gutter-sm q-mt-md">
-                <q-input v-model="newMember" label="Ajouter un membre" outlined dense />
-                <q-btn @click="addMember" icon="add" />
-              </div>
-            </div>
-
-            <div class="q-pt-md">
-              <div class="flex items-center q-gutter-sm">
-                <div class="text-bold q-mb-md">Abonnement en cours</div>
-              </div>
-              <div class="q-mb-md text-grey-8 text-caption" v-if="editedSubscription?.length === 0">
-                Ce compte n'a pas d'abonnement en cours.
-              </div>
-              <div
-                class="flex q-gutter-md q-mb-md"
-                v-for="(sub, index) in editedSubscription"
+              <q-chip
+                v-for="(member, index) in organizationDialog.organization.members"
                 :key="index"
+                removable
+                @remove="removeMember(member)"
               >
-                <q-toggle v-model="sub.active" />
-
-                <q-select
-                  style="flex: 1"
-                  outlined
-                  dense
-                  v-model="sub.plan"
-                  map-options
-                  :options="['WATCH', 'SEARCH', 'GRAPH']"
-                />
-
-                <q-input
-                  style="flex: 1; width: 100px"
-                  v-model="sub.startDate"
-                  label="Date de début"
-                  outlined
-                  dense
-                  type="date"
-                />
-
-                <q-input
-                  style="flex: 1; width: 100px"
-                  v-model="sub.endDate"
-                  label="Date de fin"
-                  outlined
-                  dense
-                  type="date"
-                />
-
-                <q-input
-                  style="flex: 1; width: 100px"
-                  v-model="sub.seats"
-                  label="Nombre de sièges"
-                  outlined
-                  dense
-                  type="number"
-                />
-
-                <q-btn flat icon="delete" @click="editedSubscription.splice(index, 1)" />
-              </div>
+                {{ member.email }}
+              </q-chip>
             </div>
-            <q-btn
-              @click="
-                editedSubscription.push({
-                  active: false,
-                  plan: '',
-                  price: 0,
-                  subscription: '',
-                  invoice: '',
-                  startDate: '',
-                  endDate: '',
-                })
-              "
-              label="Ajouter un abonnement"
-              color="primary"
-              class="q-mt-md"
-            />
-            <q-card-actions align="right" class="flex q-gutter-md">
-              <BaseButton v-close-popup secondary>Annuler</BaseButton>
-              <BaseButton @click="saveOrganization">Enregistrer</BaseButton>
-            </q-card-actions>
+            <div class="flex q-gutter-sm q-mt-md">
+              <q-input v-model="newMember" label="Ajouter un membre" outlined dense />
+              <q-btn @click="addMember" icon="add" />
+            </div>
           </q-card-section>
         </div>
+
+        <q-card-actions align="right" class="flex q-gutter-md">
+          <BaseButton v-close-popup secondary>Annuler</BaseButton>
+          <BaseButton @click="saveOrganization">Enregistrer</BaseButton>
+        </q-card-actions>
       </q-card>
     </q-dialog>
 
@@ -173,7 +100,7 @@
       <q-card>
         <q-card-section class="row items-center">
           <q-avatar icon="delete" color="negative" text-color="white" />
-          <span class="q-ml-sm">Êtes-vous sûr de vouloir supprimer cette organization ?</span>
+          <span class="q-ml-sm">Êtes-vous sûr de vouloir supprimer cette organisation ?</span>
         </q-card-section>
 
         <q-card-actions align="right">
@@ -201,9 +128,10 @@ interface Subscription {
   invoice: string;
   startDate: string;
   endDate: string;
+  seats?: number;
 }
 
-interface email {
+interface Member {
   email: string;
 }
 
@@ -211,14 +139,14 @@ interface Organization {
   _id: string;
   user_id: string;
   organization_name: string;
-  members: email[];
-  subscription: Subscription[] | null;
+  members: Member[];
+  subscription: Subscription[];
 }
 
-interface organizationDialog {
+interface OrganizationDialog {
   show: boolean;
   isEdit: boolean;
-  organization: Organization[];
+  organization: Organization;
 }
 
 interface DeleteDialog {
@@ -236,31 +164,33 @@ export default defineComponent({
     const loading = ref(false);
     const organizations = ref<Organization[]>([]);
     const router = useRouter();
-
-    const editedSubscription = ref<Subscription[]>([
-      {
-        active: false,
-        plan: '',
-        price: 0,
-        subscription: '',
-        invoice: '',
-        startDate: '',
-        endDate: '',
-      },
-    ]);
-
-    const formatDate = computed(() => (date: string) => {
-      return new Date(date).toLocaleDateString();
-    });
+    const editedSubscription = ref<Subscription[]>([]);
 
     const getMaxSeats = computed(() => {
-      console.log(organizationDialog.value.organization);
-      const seats = organizationDialog.value.organization.subscription.reduce(
-        (acc, sub) => acc + sub.seats,
-        0
-      );
+      return editedSubscription.value.reduce((acc, sub) => acc + (sub.seats || 0), 0);
+    });
 
-      return parseInt(seats);
+    const filteredOrganizations = computed(() => {
+      return organizations.value.filter((organization) =>
+        organization.organization_name.toLowerCase().includes(search.value.toLowerCase())
+      );
+    });
+
+    const organizationDialog = ref<OrganizationDialog>({
+      show: false,
+      isEdit: false,
+      organization: {
+        _id: '',
+        user_id: '',
+        organization_name: '',
+        members: [],
+        subscription: [],
+      },
+    });
+
+    const deleteDialog = ref<DeleteDialog>({
+      show: false,
+      organization: null,
     });
 
     const fetchOrganizations = async () => {
@@ -270,20 +200,11 @@ export default defineComponent({
           index: 1,
           method: 'GET',
           path: '/organizations',
-          parameters: '',
-          useQueryString: true,
-          forceRefreshToken: false,
         });
-
-        if (response && Array.isArray(response.organizations)) {
-          organizations.value = response.organizations;
-        } else {
-          console.error('Invalid response format:', response);
-        }
+        organizations.value = response.organizations || [];
       } catch (error) {
-        router.push('/');
         Notify.create({
-          message: "Vous n'avez pas les droits pour accéder à cette page",
+          message: 'Erreur lors de la récupération des organisations',
           color: 'negative',
         });
       } finally {
@@ -291,145 +212,61 @@ export default defineComponent({
       }
     };
 
-    const columns: QTableColumn[] = [
-      {
-        name: 'organization_name',
-        required: true,
-        label: 'Nom',
-        align: 'left',
-        field: 'organization_name',
-        sortable: true,
-      },
-      {
-        name: 'subscription',
-        align: 'left',
-        label: 'Statut Abonnement',
-        field: 'subscription',
-        sortable: true,
-      },
-      { name: 'actions', align: 'center', label: 'Actions', field: 'actions' },
-    ];
-
-    const filteredOrganizations = computed(() => {
-      return organizations.value.filter((organization) =>
-        organization.organization_name?.toLowerCase().includes(search.value.toLowerCase())
-      );
-    });
-
-    const organizationDialog = ref<organizationDialog>({
-      show: false,
-      isEdit: false,
-      organization: [
-        {
-          _id: '',
-          user_id: '',
-          organization_name: '',
-          members: [],
-          subscription: editedSubscription.value,
-        },
-      ],
-    });
-
-    const deleteDialog = ref<DeleteDialog>({
-      show: false,
-      organization: null,
-    });
-
     const openAddorganizationDialog = () => {
-      organizationDialog.value = {
-        show: true,
-        isEdit: false,
-        organization: {
-          _id: '',
-          user_id: '',
-          organization_name: '',
-          members: [],
-          subscription: editedSubscription.value,
-        },
-        user_groups: [],
-        groups_available: [],
-      };
+      organizationDialog.value.show = true;
+      organizationDialog.value.isEdit = false;
     };
 
-    const editOrganization = async (organization: Organization) => {
-      editedSubscription.value = JSON.parse(JSON.stringify(organization.subscription || [])) || [
-        {
-          active: false,
-          plan: '',
-          price: 0,
-          subscription: '',
-          invoice: '',
-          startDate: '',
-          endDate: '',
-        },
-      ];
-
-      for (const sub of editedSubscription.value) {
-        sub.startDate = sub.startDate?.split(' ')[0];
-        sub.endDate = sub.endDate?.split(' ')[0];
-      }
-
-      if (!organization.members) {
-        organization.members = [];
-      }
-
+    const editOrganization = (organization: Organization) => {
+      editedSubscription.value = organization.subscription;
       organizationDialog.value = {
         show: true,
         isEdit: true,
-        organization: { ...organization, subscription: editedSubscription.value },
+        organization: JSON.parse(JSON.stringify(organization)),
       };
     };
 
     const saveOrganization = async () => {
       try {
+        const { organization } = organizationDialog.value;
         await invokeApi({
           index: 1,
-          method: 'PUT',
-          path: '/organizations/' + organizationDialog.value.organization?._id,
-          parameters: {
-            organization_name: organizationDialog.value.organization?.organization_name,
-            subscription: organizationDialog.value.organization?.subscription,
-            members: organizationDialog.value.organization?.members,
-          },
-          useQueryString: false,
-          forceRefreshToken: false,
+          method: organization._id ? 'PUT' : 'POST',
+          path: `/organizations/${organization._id || ''}`,
+          parameters: organization,
         });
+        await fetchOrganizations();
+        organizationDialog.value.show = false;
       } catch (error) {
         Notify.create({
-          message: 'Une erreur est survenue lors de la modification du organization',
+          message: "Erreur lors de l'enregistrement",
           color: 'negative',
         });
       }
-      await fetchOrganizations();
-      organizationDialog.value.show = false;
     };
 
     const confirmDeleteOrganization = (organization: Organization) => {
-      deleteDialog.value = {
-        show: true,
-        organization: organization,
-      };
+      deleteDialog.value.organization = organization;
+      deleteDialog.value.show = true;
     };
 
     const deleteOrganization = async () => {
-      if (deleteDialog.value.organization) {
-        await invokeApi({
-          index: 1,
-          method: 'DELETE',
-          path: '/organizations/' + deleteDialog.value.organization._id,
-          parameters: undefined,
-          useQueryString: false,
-          forceRefreshToken: false,
+      try {
+        if (deleteDialog.value.organization) {
+          await invokeApi({
+            index: 1,
+            method: 'DELETE',
+            path: `/organizations/${deleteDialog.value.organization._id}`,
+          });
+          await fetchOrganizations();
+          deleteDialog.value.show = false;
+        }
+      } catch (error) {
+        Notify.create({
+          message: 'Erreur lors de la suppression',
+          color: 'negative',
         });
       }
-      deleteDialog.value.show = false;
-
-      await fetchOrganizations();
-    };
-
-    const removeMember = (member: email) => {
-      organizationDialog.value.organization.members =
-        organizationDialog.value.organization.members.filter((m: email) => m !== member);
     };
 
     const newMember = ref('');
@@ -442,9 +279,13 @@ export default defineComponent({
         });
         return;
       }
-
-      organizationDialog.value.organization.members.push(newMember.value);
+      organizationDialog.value.organization.members.push({ email: newMember.value });
       newMember.value = '';
+    };
+
+    const removeMember = (member: Member) => {
+      organizationDialog.value.organization.members =
+        organizationDialog.value.organization.members.filter((m) => m !== member);
     };
 
     onMounted(fetchOrganizations);
@@ -453,21 +294,19 @@ export default defineComponent({
       search,
       loading,
       organizations,
-      columns,
       filteredOrganizations,
       organizationDialog,
       deleteDialog,
-      formatDate,
-      getMaxSeats,
+      editedSubscription,
       openAddorganizationDialog,
       editOrganization,
       saveOrganization,
       confirmDeleteOrganization,
       deleteOrganization,
-      editedSubscription,
-      removeMember,
       newMember,
       addMember,
+      removeMember,
+      getMaxSeats,
     };
   },
 
